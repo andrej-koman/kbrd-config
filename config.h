170c116e0adf3bf8a1261ced05602de0edc810d5
andrej-koman <andrej.koman123@gmail.com>
2025-04-26 14:19:22 +0200

    feat: home row mods

--- config.h
+++ config.h
@@ -1,3 +1,9 @@
 #pragma once
 
 #include "keyboards/andrej/common/rgb_effects.h"
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
