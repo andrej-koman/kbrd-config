073dab6fe10f64b73aa1308524a5e57163142808
Andrej Koman <andrejkoman@hotmail.com>
2025-07-29 11:23:25 +0200

    fix: question mark back

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,
   _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______,  _______, _______,
   KC_ESC,  SI_EXLM, SI_AT,   SI_HASH, SI_DLR,  SI_PERC,                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  XXXXXXX, XXXXXXX,
-  _______, _______, _______, _______, _______, _______, _______,_______, XXXXXXX, SI_UNDS, SI_LCBR, SI_RBRC,  SI_PLUS, SI_PIPE,
+  _______, _______, _______, _______, _______, _______, _______,_______, XXXXXXX, SI_QUES, SI_LCBR, SI_RBRC,  SI_PLUS, SI_PIPE,
                              _______, _______, _______, _______, _______,  MO(_ADJUST), _______, _______
 ),
 
